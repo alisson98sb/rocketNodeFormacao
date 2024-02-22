2f93c1ae91671c5066578eb2936da9c32648d96a
alisson98sb <alisson98sb@gmail.com>
2024-02-22 15:10:07 -0300

    Finalizada aula 2 - Estrutura da aplicação

--- src/serve.js
+++ src/serve.js
@@ -1,16 +1,24 @@
 import http from 'node:http'
 
-
+const users = []
 
 const server = http.createServer((req, res) => {
     const { method, url } = req;
 
     if(method == 'GET' && url == '/users') {
-        return res.end('Listagem de usuarios') 
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users)) 
+        
     }
 
     if(method == 'POST' && url == '/users') {
-        return res.end('Criação de usuarios') 
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@gmail.com'
+        })
+        return res.writeHead(201).end() 
     }
 
     return res.end('Hello World')
