6eb16a5051a7f99266ab1bff13bd83c99635f6c5
alissonDigihub <alisson.santana@digihub.com.br>
2024-02-22 14:14:25 -0300

    fist

---
+++ src/serve.js
@@ -0,0 +1,19 @@
+import http from 'node:http'
+
+
+
+const server = http.createServer((req, res) => {
+    const { method, url } = req;
+
+    if(method == 'GET' && url == '/users') {
+        return res.end('Listagem de usuarios') 
+    }
+
+    if(method == 'POST' && url == '/users') {
+        return res.end('Criação de usuarios') 
+    }
+
+    return res.end('Hello World')
+})
+
+server.listen(3333);
